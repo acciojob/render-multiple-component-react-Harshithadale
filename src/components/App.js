06e84df4d7010b2fd0e6bed08991aaffcafbef8b
harshithadale <daleharshi6045@gmail.com>
2025-04-15 07:57:50 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,12 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <div className="ns-wrapper">
+        <h1 data-ns-test ="project-name">Project Name</h1>
+        <p data-ns-test="project-description">Project Description</p>
+      </div>
+    </div>
   )
 }
 
